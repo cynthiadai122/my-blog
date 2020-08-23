d950771895431c9df8032c25c164dd197a7716df
Cynthia <sdai10@student.monash.edu>
2020-08-23 19:08:57 +1000

    First Commit

---
+++ src/build/precache-manifest.92942b3e0178b7a0168af42399b3a886.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eae73a181bdaad4c44862d07c7fff873",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2117ee6611b80510a3d",
+    "url": "/static/css/main.84b4ca96.chunk.css"
+  },
+  {
+    "revision": "a0efec742f1befcf0b31",
+    "url": "/static/js/2.462180c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.462180c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2117ee6611b80510a3d",
+    "url": "/static/js/main.96d7323d.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
